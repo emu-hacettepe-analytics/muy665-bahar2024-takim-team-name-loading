eb05f3f19cf64548798b6d30d62e3ad3b3818f24
barissrc <164685014+barissrc@users.noreply.github.com>
2024-05-21 23:02:50 +0300

    düzenleme tahminleme eklendi

--- 9.1_Karar_Agaci_Modeli İle Tahmin.R
+++ 9.1_Karar_Agaci_Modeli İle Tahmin.R
@@ -39,50 +39,8 @@ evaluate_performance <- function(predictions, actual) {
   return(performance)
 }
 
-#Performans değerlerini yazdır
-cat("Model Performansı:\n")
-cat("Accuracy:", accuracy, "\n")
-cat("Precision:", precision, "\n")
-cat("Recall:", recall, "\n")
-cat("F1 Score:", f1_score, "\n")
+
 
 #Karar ağacını görselleştirme
 rpart.plot(model_gini, main="Kalp Hastalığı Tahminlemek İçin Karar Agacı Modeli", extra=101, under=TRUE, tweak=0.8)
 
-
-#Test seti üzerinden performans değerleme
-test_predictions <- predict(model_gini, test_data, type = "class")
-performance <- evaluate_performance(test_predictions, test_data$Kalp_Hastaligi)
-
-#performans değerlerini görselleştirme
-performance_table <- as.data.frame(performance)
-print(performance_table, row.names = TRUE)
-
-#Modeli kullanarak yeni veri ile tahmin yapma
-predict_heart_disease <- function(model, yeni_veri) {
-  prediction <- predict(model, yeni_veri, type = "class")
-  ifelse(prediction == 1, "Hasta", "Hasta Degil")
-}
-
-predict_heart_disease <- function(model) {
-  yeni_veri <- data.frame(
-    Yas = as.numeric(readline("Yaşınızı girin: ")),
-    Cinsiyet = readline("Cinsiyetinizi girin (Erkek/Kadın): "),
-    Agri_Tipi = readline("Ağrı Tipinizi girin (ATA/NAP/ASY/TA): "),
-    Dinlenme_Kan_Basinci = as.numeric(readline("Dinlenme Kan Basıncınızı girin: ")),
-    Kolestorel = as.numeric(readline("Kolesterol Değerinizi girin: ")),
-    Aclik_Kan_Sekeri = as.numeric(readline("Açlık Kan Şekerinizi girin: ")),
-    Dinlenme_EKG = readline("Dinlenme EKG Değerinizi girin (Normal/ST/LVH): "),
-    Maks_Kalp_Atis = as.numeric(readline("Maksimum Kalp Atış Sayınızı girin: ")),
-    Egzersiz_Angina = readline("Egzersiz Angina var mı? (Evet/Hayır): "),
-    Oldpeak = as.numeric(readline("Oldpeak değerinizi girin: ")),
-    ST_Egimi = readline("ST Egimi Değerinizi girin (Up/Down/Flat): ")
-  )
-  
-  prediction <- predict(model, yeni_veri, type = "class")
-  ifelse(prediction == 1, "Hasta", "Hasta Değil")
-}
-
-# Modeli kullanarak tahmin yapma
-tahmin <- predict_heart_disease(model_gini)
-print(tahmin)
